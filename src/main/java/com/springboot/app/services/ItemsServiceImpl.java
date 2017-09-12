196c3bf0c7de194dd0c9dcb53443925ca70e3797
alfonso.bravo <alfonso@abravogal.com>
2017-09-12 17:23:52 +0200

    Initial commit

--- src/main/java/com/springboot/app/services/ItemsServiceImpl.java
+++ src/main/java/com/springboot/app/services/ItemsServiceImpl.java
@@ -4,11 +4,11 @@ import com.springboot.app.persistence.mappers.ItemsMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import com.springboot.app.persistence.models.ItemsModel;
-import java.text.Normalizer;
+import com.springboot.app.utils.UtilStr;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
-
 
 @Service
 public class ItemsServiceImpl implements ItemsService
@@ -17,7 +17,6 @@ public class ItemsServiceImpl implements ItemsService
   @Autowired
   ItemsMapper iMapper;
 
-
   @Override
   public List<ItemsModel> getItemsService(ItemsModel obj) throws Exception
   {
@@ -25,7 +24,6 @@ public class ItemsServiceImpl implements ItemsService
 
     return x;
   }
-
 
   @Override
   public List<ItemsModel> searchItemsService(ItemsModel obj) throws Exception
@@ -37,109 +35,65 @@ public class ItemsServiceImpl implements ItemsService
     return list;
   }
 
-
   /**
    * METODO PARA REEMPLAZAR LA CADENA BUSCADA EN UNA LISTA DE ITEMS.
    *
    * @param obj Cadena a buscar.
-   * @param x   Lista a reemplazar.
+   * @param x Lista a reemplazar.
    *
    * @return Lista de items reemplazada.
    */
   private List<ItemsModel> replaceSearch(ItemsModel obj, List<ItemsModel> x)
   {
+    String classCss = "found";
+    List<ItemsModel> listRemove = new ArrayList<>();
+    String objSearch = UtilStr.normalizerStr(UtilStr.replaceSpacesToOneSpace(obj.getSearch()));
+    Pattern pattern = UtilStr.patternCaseInsensitive(objSearch);
+
     for (ItemsModel item : x)
     {
-      item.setNombre(this.replaceStr(obj.getNombre(), item.getNombre()));
+      boolean bln = false;
+      int iElem = 0;
+      String[] elms =
+      {
+        item.getNombre(),
+        item.getDescripcion()
+      };
 
-      item.setDescripcion(this.replaceStr(obj.getDescripcion(),
-                                          item.getDescripcion()));
+      for (String elm : elms)
+      {
+        String elmSearch = UtilStr.normalizerStr(elm);
+        Matcher compare = UtilStr.patternMatcher(pattern, elmSearch);
+
+        if (compare.find())
+        {
+          elm = elm.substring(compare.start(), compare.end());
+          String replace = compare.replaceAll(UtilStr.spanHtml(elm, classCss));
+
+          switch (iElem)
+          {
+            case 0:
+              item.setNombre(replace);
+              break;
+            case 1:
+              item.setDescripcion(replace);
+              break;
+          }
+
+          bln = true;
+        }
+
+        iElem++;
+      }
+
+      if (!bln)
+      {
+        listRemove.add(item);
+      }
     }
+
+    x.removeAll(listRemove);
 
     return x;
   }
-
-
-  /**
-   * METODO PARA REEMPLAZAR LA CADENA BUSCADA EN UN STRING.
-   *
-   * @param obj  Cadena buscada.
-   * @param item Cadena a procesar.
-   *
-   * @return Cadena reemplazada.
-   */
-  private String replaceStr(String obj, String item)
-  {
-    String replace = item;
-
-    obj = this.replaceChar(obj);
-    item = this.replaceChar(item);
-
-    Matcher x = this.pattern(obj).matcher(item);
-
-    if (x.find() && (obj.length() > 0))
-    {
-      replace = replace.substring(x.start(), x.end());
-      replace = this.pattern(obj).matcher(item)
-              .replaceAll(this.patternReplaceHtml(replace));
-    }
-
-    return replace;
-  }
-
-
-  /**
-   * METODO PARA GENERAR UN PATRON.
-   *
-   * @param str Cadena a insertar en el patron de busqueda.
-   *
-   * @return Patron generado.
-   */
-  private Pattern pattern(String str)
-  {
-    return Pattern.compile("(?i)" + str.replaceAll("\\s", ""));
-  }
-
-
-  /**
-   * METODO PARA REALIZAR UN REMPLAZO DE UNA CADENA POR UN TAG HTML.
-   *
-   * @param str Cadena a insertar en el tag.
-   *
-   * @return tag generado en html.
-   */
-  private String patternReplaceHtml(String str)
-  {
-    return "<span class=\"found\">" + str + "</span>";
-  }
-
-
-  /**
-   * CONVERSION DEL TEXTO A SU FORMA CANONICAL DECOMPOSITION, REPRESENTANDO LOS
-   * CARACTERES UTF-8 COMPRENDIDOS ENTRE U+0300 HASTA U+036F.
-   * ------------------------------------
-   * <p>
-   * "[^\\p{ASCII}]"
-   * <p>
-   * "[\\p{InCombiningDiacriticalMarks}]"
-   *
-   * -------------------------------------
-   *
-   *
-   * @param str Cadena a procesar.
-   *
-   * @return Cadena convertida a UTF-8.
-   */
-  private String replaceChar(String str)
-  {
-    String normalize = str.replace("ñ", "\001");
-
-    normalize = Normalizer.normalize(normalize, Normalizer.Form.NFD);
-    normalize = normalize.replaceAll("[\\p{InCombiningDiacriticalMarks}]", "");
-
-    normalize = normalize.replace("\001", "ñ");
-
-    return normalize;
-  }
-
 }
